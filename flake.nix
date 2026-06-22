75891b51a544d4c6aa729e782076ae3f23d7aa8c
Ardian Eka Candra <ardianoption@gmail.com>
2026-06-22 07:03:20 +0700

    fix(nix): set GSETTINGS_SCHEMA_DIR explicitly for GTK file dialog

--- flake.nix
+++ flake.nix
@@ -55,6 +55,7 @@
             export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules"
             export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
             export XDG_DATA_DIRS="${pkgs.adwaita-icon-theme}/share:${pkgs.gsettings-desktop-schemas}/share:${pkgs.shared-mime-info}/share:$XDG_DATA_DIRS"
+            export GSETTINGS_SCHEMA_DIR="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
           '';
         };
       }
