077f43b2299cc0480889d41d47faf1855229ae82
Ardian Eka Candra <ardianoption@gmail.com>
2026-07-06 08:30:48 +0700

    refactor(sakti): delete orphaned references module + test (#references)

--- packages/sakti/src/core/index.ts
+++ packages/sakti/src/core/index.ts
@@ -1,6 +1,5 @@
 export { GLOBAL_DATA_DIR_NAME, type GlobalDataDirOptions, getGlobalDataDir } from './global-config.js';
 
-export * from './references.js';
 export * from './store/index.js';
 export * from './planning-home.js';
 export * from './sakti-root.js';
